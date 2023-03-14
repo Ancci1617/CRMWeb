ffbffe6d4eea1fc1a64a3a3e8d248afbf75f6acc
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-03-14 16:31:58 -0300

    Coreccion de las FECHAS en ventas

--- src/model/CRM/get_tablas/get_fichas.js
+++ src/model/CRM/get_tablas/get_fichas.js
@@ -5,14 +5,16 @@ const pool = require("../../connection-database.js");
 const getFichas = async (cte) => {
 
     const [rows] = await pool.query(
-        // "SET lc_time_names = 'es_ES'; "+ 
+
         "SELECT DATE_FORMAT(FECHA,'%d/%m/%y'),Ficha,Z,VTA,`Anterior`,`Mes 0`,`Mes 1`,`Mes 2`,`Mes 3`,`Mes 4`,`Mes 5`, " +
         "`Sal Ant`,`Mes 6`,`Sal Act`,Cuota,`Valor Unitario`, " +
         "DATE_FORMAT(`VEN`,'%d-%b') as VEN, DATE_FORMAT(VenMovil,'%d-%b') as VenMovil,Vencidas,`Cuotas Pagas`,Totales,Atraso from Cobranzas where CTE = ? " +
         "UNION " +
-        "SELECT DATE_FORMAT(`Fecha`, '%d/%M/%y'),`Ficha`,`Zona`,`Total`,0,0,0,0,0,0,0,Total,Cobrado,Saldo,Cuota,`Valor Unitario`, " +
+        "SELECT DATE_FORMAT(`Fecha`, '%d/%m/%y'),`Ficha`,`Zona`,`Total`,null,null,null,null,null,null,null,Total,Cobrado,Saldo,Cuota,`Valor Unitario`, " +
         "DATE_FORMAT(PrimerVencimiento,'%d-%b') as PrimerVencimiento,DATE_FORMAT(`Fecha cobro`,'%d-%b') as `Fecha cobro`,Vencidas,`Cuotas Pagas`,`Cuotas`,`Atraso` FROM Ventas where CTE = ?", [cte, cte]);
-
+    
+        console.log(rows);
+        
     if (rows.length > 0) {
         return rows;
     }
