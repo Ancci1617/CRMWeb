eb4c123395421da59531628ce02b9aaa56b17719
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-03-23 09:51:43 -0300

    Correcion bug no puede cargar mercaderia

--- src/public/js/mercaderia/planilla.js
+++ src/public/js/mercaderia/planilla.js
@@ -1,7 +1,7 @@
 let options_selector = document.querySelectorAll("select.option-input");
 let input = document.querySelectorAll("input.hidden.select_input");
 let btn_actualiza = document.querySelector(".btn-actualiza");
-const form_planilla = document.querySelector("form");
+const form_planilla = document.querySelector(".form-carga");
 
 
 //Asociar hidden input con select
@@ -11,7 +11,7 @@ for(i = 0; i < options_selector.length; i++){
 
 
 btn_actualiza.addEventListener("click", e=> {
-
+    console.log("n");
     form_planilla.submit(); 
 
    
