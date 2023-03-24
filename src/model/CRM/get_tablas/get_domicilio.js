2f3fb67bdc21102d8ef067361de4ff9cffdb9402
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-03-24 09:50:01 -0300

    el primer vencimiento deja de tener el mes de la fecha de venta y pasa a tener el mes del primer pago

--- src/model/CRM/get_tablas/get_domicilio.js
+++ src/model/CRM/get_tablas/get_domicilio.js
@@ -6,7 +6,7 @@ const getDomicilio = async (calle) => {
         "SELECT MasterResumen.CALIF, Clientes.`APELLIDO Y NOMBRE`,Clientes.CTE, " + 
         "Clientes.`FICHA`, Cobranzas.Atraso FROM`Clientes` left join " +
         "MasterResumen on MasterResumen.Cliente = Clientes.CTE left join " + 
-        "Cobranzas on Cobranzas.Ficha = Clientes.FICHA WHERE Clientes.calle = ?"
+        "Cobranzas on Cobranzas.Ficha = Clientes.FICHA WHERE Clientes.calle = ? order by Clientes.CTE"
         , [calle]);
 
     if (rows.length > 0) {
--- src/model/ventas/archivo_de_entrega.js
+++ src/model/ventas/archivo_de_entrega.js
@@ -6,7 +6,7 @@ async function getArchivoDeEntregaVentas(vendedor,dia) {
         "CUOTA,null as n5,'formula' as n6,'formula' as n7,'formula' as n8,tipo,ESTATUS,PRIMER_PAGO,NULL as n9,NULL as n10, " + 
         "'formula' as n11,'formula' as n12,'formula' as n13,'formula' as n14,'formula' as n15,'formula' as n16 ,'formula' as n17," + 
         "'formula' as n18,'formula' as n19,'formula' as n20,'formula' as n21,null as n22, " + 
-        "Concat(year(FECHA_VENTA),'-',Month(FECHA_VENTA),'-',VENCIMIENTO) as VENCIMIENTO from VentasCargadas where " + 
+        "Concat(year(PRIMER_PAGO),'-',Month(PRIMER_PAGO),'-',VENCIMIENTO) as VENCIMIENTO from VentasCargadas where " + 
         "USUARIO = ? and FECHA_VENTA = ? and VISIBLE = 1 order by FICHA", [vendedor,dia])
 
     if (ventas.length > 0) {
