8daf5862d7cce0ff1e45e70e5e8f3227fd50b41f
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-04-17 15:10:35 -0300

    Order by en Carga de la camioneta

--- src/model/mercaderia/camionetas/camionetas.model.js
+++ src/model/mercaderia/camionetas/camionetas.model.js
@@ -2,7 +2,7 @@ const pool = require("../../connection-database");
 
 const getCargaVigente = async (UNIDAD) => {
     const [stock] = await pool.query(
-        "SELECT DISTINCT ART, SUM(EFECTO_UNIDAD) as VIGENTE FROM `STOCK` WHERE CAMIONETA = ? group by ART; "
+        "SELECT DISTINCT ART, SUM(EFECTO_UNIDAD) as VIGENTE FROM `STOCK` WHERE CAMIONETA = ? group by ART ORDER BY VIGENTE; "
         , [UNIDAD]);
 
     return stock;
