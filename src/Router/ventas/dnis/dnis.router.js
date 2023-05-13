3f3297ece55a8f098d9a1a64b7b07d5af95e4afb
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-05-13 13:04:14 -0300

    ordenamiento

--- src/Router/ventas/dnis/dnis.router.js
+++ src/Router/ventas/dnis/dnis.router.js
@@ -9,7 +9,11 @@ const getDirectories = async src => (await fs.readdir(src, { withFileTypes: true
 
 Router.get("/dnis",async (req, res) => {
     const dirs = await getDirectories(path.join("..","ImagenesDeClientes"));
-    const dirs_ordered = dirs.map( e => parseInt(e) ).filter(e => !isNaN(e)).sort();
+    const dirs_ordered = dirs.map( e => parseInt(e) ).filter(e => !isNaN(e)).sort((a,b)=>{
+        if(a < b) return -1;
+        if(b < a) return 1;
+        return 0;
+    });
     const links = dirs_ordered.map(dir_name => `/dnis/${dir_name}`);
     const data = { title : "Dnis" , items : dirs_ordered,links };
     res.render("list-items.ejs", { data });
