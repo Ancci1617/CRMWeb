2b5c7e6f88ad3aed6694bd76d8e41978ee782e33
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-05-31 08:40:46 -0300

    Mejora frontend de pedidos__generales

--- src/public/js/pedidos/pedidos.generales.activos.js
+++ src/public/js/pedidos/pedidos.generales.activos.js
@@ -2,42 +2,31 @@ const grupos = document.querySelectorAll(".grupo");
 const buscador = document.querySelector(".buscador");
 
 
-function actualizarTotales(){
+function actualizarTotales() {
     grupos.forEach(grupo => {
         const cantidad = grupo.querySelectorAll(".grupo__data:not(.hidden)").length;
-        grupo.querySelector(".detalle").innerText = `Total : (${cantidad})`;
+        grupo.querySelector(".detalle").innerText = `Total: (${cantidad})`;
     });
 }
 
 buscador.addEventListener("change", e => {
-    const data = e.target.value;
-    
-    grupos.forEach(grupo => {
-        const grupos__data = grupo.querySelectorAll(".grupo__data");
+    const data = e.target.value.toUpperCase();
+    const grupos_data = document.querySelectorAll(".grupo__data");
+    grupos_data.forEach(grupo__data => {
 
-        grupos__data.forEach(grupo__data => {
-            
-            if (!grupo__data.innerHTML.includes(data)) {
-                grupo__data.classList.add("hidden");
-            } else {
-                grupo__data.classList.remove("hidden");
-            }
-
-        });
-
-
-
+        if (!grupo__data.innerHTML.toUpperCase().includes(data))
+            return grupo__data.classList.add("hidden");
+        grupo__data.classList.remove("hidden");
     });
 
-    buscador.innerHTML.includes(data);
     actualizarTotales();
 });
 
 //En cada grupo, le agrega un listener click en title
 grupos.forEach(grupo => {
     grupo.querySelector(".grupo__title").addEventListener("click", e => {
-        const inner_Data__container = grupo.querySelector(".data__container");
-        inner_Data__container.classList.toggle("hidden");
+        const data__container = grupo.querySelector(".data__container");
+        data__container.classList.toggle("hidden");
     });
 });
 
