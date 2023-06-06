171c114d93c181f0e29e6c2e662cdcc371a08c01
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-06-06 12:07:12 -0300

    Archivo de entrega, logica de fechas cambiada

--- src/model/ventas/archivo_de_entrega.js
+++ src/model/ventas/archivo_de_entrega.js
@@ -1,13 +1,17 @@
 const pool = require("../../model/connection-database");
 
-async function getArchivoDeEntregaVentas(vendedor,dia) {
+async function getArchivoDeEntregaVentas(vendedor, dia) {
     const [ventas] = await pool.query(
-        "Select FECHA_VENTA,CTE,FICHA,ZONA,TOTAL,null as n1,null as n2,'formula' as n3,'formula' as n4, "+
-        "CUOTA,null as n5,'formula' as n6,'formula' as n7,'formula' as n8,tipo,ESTATUS,PRIMER_PAGO,NULL as n9,NULL as n10, " + 
-        "'formula' as n11,'formula' as n12,'formula' as n13,'formula' as n14,'formula' as n15,'formula' as n16 ,'formula' as n17," + 
-        "'formula' as n18,'formula' as n19,'formula' as n20,'formula' as n21,null as n22, " + 
-        "Concat(year(PRIMER_PAGO),'-',Month(PRIMER_PAGO),'-',VENCIMIENTO) as VENCIMIENTO from VentasCargadas where " + 
-        "USUARIO = ? and FECHA_VENTA = ? and VISIBLE = 1 order by FICHA", [vendedor,dia])
+        "Select FECHA_VENTA, CTE, FICHA, ZONA, TOTAL, null as n1, "+
+        "null as n2, 'formula' as n3, 'formula' as n4, CUOTA, null as n5, 'formula' as n6, "+
+        "'formula' as n7, 'formula' as n8, tipo, ESTATUS, PRIMER_PAGO, NULL as n9, NULL as n10, "+
+        "'formula' as n11, 'formula' as n12, 'formula' as n13, 'formula' as n14, 'formula' as n15, "+
+        "'formula' as n16, 'formula' as n17, 'formula' as n18, 'formula' as n19, 'formula' as n20, "+
+        "'formula' as n21, null as n22, " +
+        "if (dateDiff(Concat(year(FECHA_VENTA), '-', Month(FECHA_VENTA) + 1, VENCIMIENTO), FECHA_VENTA) >= 32, "+
+        "Concat(year(FECHA_VENTA), '-', Month(FECHA_VENTA), '-', VENCIMIENTO), Concat(year(FECHA_VENTA), '-', "+
+        "Month(FECHA_VENTA) + 1, '-', VENCIMIENTO)) as VENCIMIENTO " + 
+        "from VentasCargadas where USUARIO = ? and FECHA_VENTA = ? and VISIBLE = 1 order by FICHA;", [vendedor, dia])
 
     if (ventas.length > 0) {
         return ventas;
@@ -17,12 +21,12 @@ async function getArchivoDeEntregaVentas(vendedor,dia) {
 }
 
 
-async function getArchivoDeEntregaAccess(vendedor,dia) {
+async function getArchivoDeEntregaAccess(vendedor, dia) {
     const [ventas] = await pool.query(
-        "Select FECHA_VENTA,CTE,FICHA,NOMBRE,ZONA,CALLE,CRUCES,CRUCES2,WHATSAPP, "+
-        "DNI,ARTICULOS,TOTAL,ANTICIPO,CUOTA,TIPO,ESTATUS,PRIMER_PAGO,Usuario from VentasCargadas "+
+        "Select FECHA_VENTA,CTE,FICHA,NOMBRE,ZONA,CALLE,CRUCES,CRUCES2,WHATSAPP, " +
+        "DNI,ARTICULOS,TOTAL,ANTICIPO,CUOTA,TIPO,ESTATUS,PRIMER_PAGO,Usuario from VentasCargadas " +
         "where USUARIO = ? and FECHA_VENTA = ? and VISIBLE = 1 order by FICHA"
-        ,[vendedor,dia])
+        , [vendedor, dia])
 
     if (ventas.length > 0) {
         return ventas;
@@ -33,5 +37,5 @@ async function getArchivoDeEntregaAccess(vendedor,dia) {
 
 
 
-module.exports = {getArchivoDeEntregaVentas, getArchivoDeEntregaAccess}
+module.exports = { getArchivoDeEntregaVentas, getArchivoDeEntregaAccess }
 
