8afe35379bda255e6e984b03990b36ed4f952ccf
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-06-07 09:15:54 -0300

    Vuelve a db original

--- src/model/connection-object.js
+++ src/model/connection-object.js
@@ -1,18 +1,18 @@
-// const connection_data = {
-//     host: 'sql815.main-hosting.eu',
-// 	port: 3306,
-// 	user: 'u970133903_BGMAdmin',
-// 	password: 'RKfwnmPhW1!l',
-// 	database: 'u970133903_BGMDB'
-// }
-
-//DE PRUEBA
 const connection_data = {
     host: 'sql815.main-hosting.eu',
 	port: 3306,
-	user: 'u970133903_BGMTest',
-	password: 'h2?Fx3@E',
-	database: 'u970133903_BGMTest'
+	user: 'u970133903_BGMAdmin',
+	password: 'RKfwnmPhW1!l',
+	database: 'u970133903_BGMDB'
 }
+
+//DE PRUEBA
+// const connection_data = {
+//     host: 'sql815.main-hosting.eu',
+// 	port: 3306,
+// 	user: 'u970133903_BGMTest',
+// 	password: 'h2?Fx3@E',
+// 	database: 'u970133903_BGMTest'
+// }
 
 module.exports = connection_data;
