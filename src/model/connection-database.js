536a075582c80e64f843872cbb31df3ed8233d02
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-06-07 09:26:25 -0300

    Promesa pool

--- src/model/connection-database.js
+++ src/model/connection-database.js
@@ -1,12 +1,12 @@
-const mysql = require("mysql2/promise.js")
+const mysql = require("mysql2")
 const objConnection = require("./connection-object.js")
 
 
 try {    
-        
+    
     const pool = mysql.createPool(objConnection);
     
-    module.exports = pool;
+    module.exports = pool.promise();
 
 
 } catch (error) {
@@ -16,7 +16,24 @@ try {
 
 }
 
+// export async function connect(){
 
+//     // If the pool was already created, return it instead of creating a new one.
+//     if(typeof globalPool !== 'undefined') {
+//       return globalPool;
+//     }
+  
+//     // If we have gotten this far, the pool doesn't exist, so lets create one.
+//     globalPool = await mysql.createPool({
+//       host: 'localhost',
+//       port: 3306,
+//       user: 'root',
+//       password: '',
+//       database: 'mendozarq',
+//       connectionLimit: 10
+//     });
+//     return globalPool;
+//   }
 
 
 
