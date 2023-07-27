29f1dfcb35ad6c4275e00b4ef416ae84aba62dc3
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-07-27 12:34:53 -0300

    Mueve consulta de listado a archivo sql

--- src/middlewares/permission.middleware.js
+++ src/middlewares/permission.middleware.js
@@ -1,16 +1,11 @@
 
 function hasPermission(permiso) {
     return function (req, res, next) {
-
-        if (req.user.PERMISOS.includes("*")) return next();
-
-        if (!req.user.PERMISOS.includes(permiso))
-                return res.status(403).send('No tenes permiso para acceder a esta funcionalidad.');
-
-        next();
-    };
+        if (res.locals.hasPermission(permiso)) return next();
+        
+        return res.status(403).send('No tenes permiso para acceder a esta funcionalidad.');
+    }
 }
-
 
 
 module.exports = { hasPermission }
