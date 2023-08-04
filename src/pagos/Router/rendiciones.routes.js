25c5406c4e8c12d8b6c81e43e80299bbc575ced6
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-08-04 15:25:49 -0300

    rendiciones

--- src/pagos/Router/rendiciones.routes.js
+++ src/pagos/Router/rendiciones.routes.js
@@ -1,15 +1,18 @@
 "use strict";
 
 const Router = require("express").Router();
-const {rendicionReceptor,generarRendicion,cargarEfectivo,updateEditable} = require("../controller/rendicion.controller.js")
+const {rendicionReceptor,generarRendicion,cargarEfectivo,updateEditable,cargarGasto,borrarGasto} = require("../controller/rendicion.controller.js")
 
 
 
 
 Router.get("/rendicion_receptor", rendicionReceptor);
 Router.get("/generar_rendicion", generarRendicion);
+Router.get("/borrar_gasto",borrarGasto)
 Router.post("/cargar_efectivo",cargarEfectivo);
 Router.post("/cambiar_editable",updateEditable);
+Router.post("/cargar_gasto",cargarGasto);
+
 
 
 
--- src/pagos/controller/rendicion.controller.js
+++ src/pagos/controller/rendicion.controller.js
@@ -5,7 +5,7 @@ const { pagosModel } = require("../model/pagos.model.js");
 async function cargarEfectivo(req, res) {
     const { ID, EFECTIVO = 0 } = req.body;
     try {
-        const [efectivo] = await pool.query("UPDATE `PlanillasDeCobranza` SET EFECTIVO = ? WHERE ID = ?", [EFECTIVO, ID]);
+        const [efectivo] = await pool.query("UPDATE `PlanillasDeCobranza` SET EFECTIVO = ? WHERE ID = ? AND EDITABLE = 1", [EFECTIVO, ID]);
     } catch (error) {
         console.log(error);
         return res.send("No se pudo cargar el efectivo..");
@@ -13,16 +13,37 @@ async function cargarEfectivo(req, res) {
     res.redirect(req.headers.referer);
 }
 async function updateEditable(req, res) {
-    const { ID,EDITABLE = 0 } = req.body;
-    console.log("update editable",req.body);
+    const { ID, EDITABLE = 0 } = req.body;
+    console.log("update editable", req.body);
     try {
-        const [res] = await pool.query("UPDATE `PlanillasDeCobranza` SET ? WHERE ID = ?", [{EDITABLE},ID]);
+        const [res] = await pool.query("UPDATE `PlanillasDeCobranza` SET ? WHERE ID = ?", [{ EDITABLE }, ID]);
     } catch (error) {
         console.log(error);
     }
-    console.log("refer",req.headers.referer);
     res.redirect(req.headers.referer);
 }
+
+async function cargarGasto(req, res) {
+    const { GASTO = null, MONTO = 0, ID_RENDICION = -1, OBS } = req.body;
+
+    console.log("body", req.body);
+    try {
+        const [res] = await pool.query(
+            "INSERT INTO Gastos (GASTO,MONTO,ID_RENDICION,OBS) " +
+            "SELECT ? FROM PlanillasDeCobranza " +  
+            "WHERE PlanillasDeCobranza.ID = ? AND PlanillasDeCobranza.EDITABLE = 1; ",
+            [[GASTO, MONTO, ID_RENDICION, OBS],ID_RENDICION]);
+            
+        console.log("cargar gasto", res);
+    } catch (error) {
+        console.log(error);
+    }
+
+    res.redirect(req.headers.referer);
+
+}
+
+
 
 async function rendicionReceptor(req, res) {
 
@@ -38,22 +59,13 @@ async function rendicionReceptor(req, res) {
     });
 
     const [rendicion] = await pool.query(
-        "SELECT Pl.ID,`COB`, Pl.`FECHA`, `EFECTIVO`, `RECEPCION`, `EDITABLE`, " +
-        "SUM(Pa.VALOR + Pa.MORA + Pa.SERV)  AS TOTAL_COBRADO, " +
-        "SUM(CASE WHEN Pa.MP_OPERACION IS NOT NULL > 0 THEN Pa.MP ELSE 0 END) as MP, " +
-        "SUM(Pa.VALOR + Pa.MORA + Pa.SERV) - EFECTIVO as DIFERENCIA, " +
-        "SUM(Pa.VALOR + Pa.MORA + Pa.SERV) - EFECTIVO AS TOTAL_GASTOS " +
-        "FROM `PlanillasDeCobranza` Pl " +
-        "Left join PagosSV Pa on Pa.FECHA = Pl.FECHA AND Pa.COBRADOR = Pl.COB " +
-        "WHERE Pl.FECHA = ? and Pl.COB = ?;", [FECHA, COB]);
+        "SELECT Pl.ID,`COB`, Pl.`FECHA`, `EFECTIVO`, `RECEPCION`, `EDITABLE`, SUM(Pa.VALOR + Pa.MORA + Pa.SERV) AS TOTAL_COBRADO, SUM(CASE WHEN Pa.MP_OPERACION IS NOT NULL > 0 THEN Pa.MP ELSE 0 END) as MP, IFNULL((SELECT SUM(MONTO) from Gastos where ID_RENDICION = Pl.ID),0) AS TOTAL_GASTOS , SUM(Pa.VALOR + Pa.MORA + Pa.SERV) - SUM(CASE WHEN Pa.MP_OPERACION IS NOT NULL > 0 THEN Pa.MP ELSE 0 END) - IFNULL((SELECT SUM(MONTO) from Gastos where ID_RENDICION = Pl.ID),0) - EFECTIVO as DIFERENCIA FROM `PlanillasDeCobranza` Pl Left join PagosSV Pa on Pa.FECHA = Pl.FECHA AND Pa.COBRADOR = Pl.COB WHERE Pl.FECHA = ? and Pl.COB = ?;", [FECHA, COB]);
 
-
-    const gastos = [{ GASTO: "ADELANTO", MONTO: 200, OBS: "obs" }];
-
-    if (req.user.Usuario == rendicion[0].COB) {
-        // return res.render("");
-    }
-
+    console.log("rendicion",rendicion);
+    // const gastos = [{ GASTO: "ADELANTO", MONTO: 200, OBS: "obs" }];
+    const [gastos] = await pool.query(
+        "SELECT * from Gastos where ID_RENDICION = (SELECT ID FROM PlanillasDeCobranza WHERE FECHA = ? and COB = ?);", [FECHA, COB])
+    
     res.render("pagos/rendiciones/rendicion.receptor.ejs", { aside: render_links, rendicion: rendicion[0], gastos, FECHA, COB });
 
 
@@ -72,7 +84,19 @@ async function generarRendicion(req, res) {
 
 }
 
+async function borrarGasto(req,res){
+    const {ID,FECHA,COB} = req.query;
+    console.log(req.headers);
+    
+    try {
+        const [gasto_eliminado] = await pool.query("DELETE FROM Gastos where ID = ?",[ID]);
+        console.log("gasto eliminado",gasto_eliminado);
+    } catch (error) {
+        console.error(error);
+    }
+
+    res.redirect(`/rendicion/rendicion_receptor?FECHA=${FECHA}&COB=${COB}`);
+}
 
 
-
-module.exports = { rendicionReceptor, generarRendicion, cargarEfectivo ,updateEditable};
+module.exports = { rendicionReceptor, generarRendicion, cargarEfectivo, updateEditable, cargarGasto ,borrarGasto};
