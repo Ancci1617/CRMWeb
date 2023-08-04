3ae0e69c5da3d11dea136a60e689c6eb691ef90d
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-08-04 17:34:40 -0300

    Deuda de servicio en funcion del rango, + Comentado las 2 opciones

--- src/lib/doubt.js
+++ src/lib/doubt.js
@@ -1,7 +1,7 @@
 const { getVencidas, getToday, sumarMeses } = require("../lib/dates");
 
 function getDoubt({ VENCIMIENTO, CUOTAS, CUOTA, TOTAL, CUOTA_ANT, CUOTA_PAGO, SALDO,
-    SERVICIO_ANT, SERV_PAGO, SERV_UNIT, MORA_ANT, MORA_PAGO }) {
+    SERVICIO_ANT, SERV_PAGO, SERV_UNIT, MORA_ANT, MORA_PAGO},COBRADOR = false ) {
     //AGREGAR ALGORITMO PARA COBRADOR
 
 
@@ -14,15 +14,32 @@ function getDoubt({ VENCIMIENTO, CUOTAS, CUOTA, TOTAL, CUOTA_ANT, CUOTA_PAGO, SA
 
     const atraso = parseFloat(Math.max(vencidas - pagas, 0).toFixed(1));
 
-    const atraso_eval = Math.max(Math.ceil(vencidas - (pagas + 0.3)), 0);
+    let atraso_eval = Math.max(Math.ceil(vencidas - (pagas + 0.3)), 0);
 
-    const deuda_serv = Math.max(SERVICIO_ANT - SERV_PAGO + atraso_eval * SERV_UNIT, 0);
 
     const deuda_mora = MORA_ANT - MORA_PAGO + Math.max(atraso_eval - 1, 0) * CUOTA * 0.1;
+
+    //opcion 1, si esta al dia, agregale el servicio de este mes, SINO, DEJAR COMO ESTA (
+    // En caso que pase esto, revisar el caso: (Esta atrasada, paga el serv del mes pasado, la cuota del mes pasado y la cuota de este mes, se le cobra el serv de este mes?)
+    //)
+
+    //opcion 2(vigente), siempre agregar el servicio de este mes 
+            
+    //Si no le vencio este mes, agrega 1 servicio ( Esto despues de calcular la mora Q)
+    if (vencidas < CUOTAS &&
+        COBRADOR &&
+        getToday() < `${VENCIMIENTO.split("-")[0]}-${getToday().split("-")[1]}-${VENCIMIENTO.split("-")[2]}`) {
+        atraso_eval = atraso_eval + 1;
+    }
+
+    console.log("atraso eval",atraso_eval);
+    const deuda_serv = Math.max(SERVICIO_ANT - SERV_PAGO + atraso_eval * SERV_UNIT, 0);
 
     console.log("VENCIMIENTO", VENCIMIENTO);
 
     console.log("SUMARMESES", sumarMeses(new Date(VENCIMIENTO), Math.floor(pagas)));
+
+
 
 
     const vencimiento_vigente = sumarMeses(new Date(VENCIMIENTO), Math.floor(pagas)).toISOString().split("T")[0];
