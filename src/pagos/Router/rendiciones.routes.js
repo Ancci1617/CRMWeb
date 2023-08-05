0b08d60620473e588f8f2ecce56dbd62a765fb26
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-08-05 15:36:55 -0300

    Cargar

--- src/pagos/Router/rendiciones.routes.js
+++ src/pagos/Router/rendiciones.routes.js
@@ -8,7 +8,7 @@ const {PAGOS_ADMIN,PAGOS_USER} = require("../../constants/permisos.js");
 
 Router.get("/rendicion_receptor",hasPermission(PAGOS_USER),rendicionReceptor);
 Router.get("/generar_rendicion", hasPermission(PAGOS_ADMIN),generarRendicion);
-Router.get("/borrar_gasto",hasPermission(PAGOS_ADMIN),borrarGasto)
+Router.get("/borrar_gasto",hasPermission(PAGOS_USER),borrarGasto)
 Router.post("/cargar_efectivo",hasPermission(PAGOS_ADMIN),cargarEfectivo);
 Router.post("/cambiar_editable",hasPermission(PAGOS_ADMIN),updateEditable);
 Router.post("/cargar_gasto",hasPermission(PAGOS_USER),cargarGasto);
