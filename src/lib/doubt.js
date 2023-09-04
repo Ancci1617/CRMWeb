d4a50afa3622b836efa0ddd1e236d9c1a34a8b4a
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-09-04 16:15:29 -0300

    El vencimiento adicional no es < sino <=

--- src/lib/doubt.js
+++ src/lib/doubt.js
@@ -37,7 +37,7 @@ function getDoubt({ VENCIMIENTO, PRIMER_PAGO, CUOTAS, CUOTA, TOTAL, CUOTA_ANT, C
     //Si no le vencio este mes, agrega 1 servicio ( Esto despues de calcular la mora Q)
     let deuda_serv = FECHA_VENTA < '2022-12-01' ? 0 : Math.max(SERVICIO_ANT - SERV_PAGO + atraso_eval * SERV_UNIT, 0);
     if (vencidas < CUOTAS && COBRADOR &&
-        getToday() < `${VENCIMIENTO_EVALUA.split("-")[0]}-${getToday().split("-")[1]}-${VENCIMIENTO_EVALUA.split("-")[2]}`
+        getToday() <= `${VENCIMIENTO_EVALUA.split("-")[0]}-${getToday().split("-")[1]}-${VENCIMIENTO_EVALUA.split("-")[2]}`
         && !zonas_sin_servicio_cobranza.includes(Z) && !deuda_serv > 0
     ) {
         atraso_eval = atraso_eval + 1;
@@ -56,7 +56,8 @@ function getDoubt({ VENCIMIENTO, PRIMER_PAGO, CUOTAS, CUOTA, TOTAL, CUOTA_ANT, C
 
     console.log("ant", SERVICIO_ANT);
     console.log("PAGO", SERV_PAGO);
-
+    console.log("vencimiento",VENCIMIENTO);
+    console.log("pagas",pagas);
     const vencimiento_vigente = sumarMeses(new Date(VENCIMIENTO), Math.floor(pagas)).toISOString().split("T")[0];
     console.log("atraso eval", atraso_eval);
     return {
