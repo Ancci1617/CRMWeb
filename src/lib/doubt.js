b32105ff491aa8232f1bbd2d80a3a394b5f0ed6a
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-09-07 12:07:23 -0300

    Ficha vieja abona servicio y mora

--- src/lib/doubt.js
+++ src/lib/doubt.js
@@ -29,19 +29,23 @@ function getDoubt({ VENCIMIENTO, PRIMER_PAGO, CUOTAS, CUOTA, TOTAL, CUOTA_ANT, C
     let atraso_eval = Math.max(Math.ceil(vencidas - (pagas + 0.3)), 0);
 
 
-    const deuda_mora = FECHA_VENTA < '2022-12-01' ? 0 : Math.max(MORA_ANT - MORA_PAGO + Math.max(atraso_eval - 1, 0) * CUOTA * 0.1, 0);
+    // const deuda_mora = FECHA_VENTA < '2022-12-01' ? 0 : Math.max(MORA_ANT - MORA_PAGO + Math.max(atraso_eval - 1, 0) * CUOTA * 0.1, 0);
+    const deuda_mora = Math.max(MORA_ANT - MORA_PAGO + Math.max(atraso_eval - 1, 0) * CUOTA * 0.1, 0);
 
 
 
 
     //Si no le vencio este mes, agrega 1 servicio ( Esto despues de calcular la mora Q)
-    let deuda_serv = FECHA_VENTA < '2022-12-01' ? 0 : Math.max(SERVICIO_ANT - SERV_PAGO + atraso_eval * SERV_UNIT, 0);
+    // let deuda_serv = FECHA_VENTA < '2022-12-01' ? 0 : Math.max(SERVICIO_ANT - SERV_PAGO + atraso_eval * SERV_UNIT, 0);
+    let deuda_serv = Math.max(SERVICIO_ANT - SERV_PAGO + atraso_eval * SERV_UNIT, 0);
+
     if (vencidas < CUOTAS && COBRADOR &&
         getToday() <= `${VENCIMIENTO_EVALUA.split("-")[0]}-${getToday().split("-")[1]}-${VENCIMIENTO_EVALUA.split("-")[2]}`
         && !zonas_sin_servicio_cobranza.includes(Z) && !deuda_serv > 0
     ) {
         atraso_eval = atraso_eval + 1;
-        deuda_serv = FECHA_VENTA < '2022-12-01' ? 0 : Math.max(SERVICIO_ANT - SERV_PAGO + atraso_eval * SERV_UNIT, 0);
+        // deuda_serv = FECHA_VENTA < '2022-12-01' ? 0 : Math.max(SERVICIO_ANT - SERV_PAGO + atraso_eval * SERV_UNIT, 0);
+        deuda_serv = Math.max(SERVICIO_ANT - SERV_PAGO + atraso_eval * SERV_UNIT, 0);
 
     }
 
