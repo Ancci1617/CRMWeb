ff85859d553eead561161121845743a35e18b5f3
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-09-20 17:32:33 -0300

    #Bug fix - Muestra un vencimiento en archivo de entrega que no corresponde

--- src/model/ventas/archivo_de_entrega.js
+++ src/model/ventas/archivo_de_entrega.js
@@ -2,16 +2,14 @@ const pool = require("../../model/connection-database");
 
 async function getArchivoDeEntregaVentas(vendedor, dia) {
     const [ventas] = await pool.query(
-        "Select FECHA_VENTA, CTE, FICHA, ZONA, TOTAL, null as n1, "+
-        "null as n2, 'formula' as n3, 'formula' as n4, CUOTA, null as n5, 'formula' as n6, "+
-        "'formula' as n7, 'formula' as n8, tipo, ESTATUS, PRIMER_PAGO, NULL as n9, NULL as n10, "+
+        "Select FECHA_VENTA, VentasCargadas.CTE, VentasCargadas.FICHA, ZONA, VentasCargadas.TOTAL, null as n1, "+
+        "null as n2, 'formula' as n3, 'formula' as n4, VentasCargadas.CUOTA, null as n5, 'formula' as n6, "+
+        "'formula' as n7, 'formula' as n8, tipo, ESTATUS, Fichas.PRIMER_PAGO, NULL as n9, NULL as n10, "+
         "'formula' as n11, 'formula' as n12, 'formula' as n13, 'formula' as n14, 'formula' as n15, "+
         "'formula' as n16, 'formula' as n17, 'formula' as n18, 'formula' as n19, 'formula' as n20, "+
         "'formula' as n21, null as n22, " +
-        "if (dateDiff(Concat(year(FECHA_VENTA), '-', Month(FECHA_VENTA) + 1, VENCIMIENTO), FECHA_VENTA) >= 32, "+
-        "Concat(year(FECHA_VENTA), '-', Month(FECHA_VENTA), '-', VENCIMIENTO), Concat(year(FECHA_VENTA), '-', "+
-        "Month(FECHA_VENTA) + 1, '-', VENCIMIENTO)) as VENCIMIENTO " + 
-        "from VentasCargadas where USUARIO = ? and FECHA_VENTA = ? and VISIBLE = 1 order by FICHA;", [vendedor, dia])
+        " Fichas.VENCIMIENTO " + 
+        "from VentasCargadas LEFT JOIN Fichas on Fichas.ID_VENTA = VentasCargadas.INDICE where USUARIO = ? and FECHA_VENTA = ? and VISIBLE = 1 order by FICHA;", [vendedor, dia])
 
     if (ventas.length > 0) {
         return ventas;
