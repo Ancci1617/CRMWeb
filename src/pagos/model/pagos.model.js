5928bc88c80d88f0662a9cfacd3d846f4fc1b241
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-09-22 09:16:39 -0300

    PagosModel no es una instancia sino una clase

--- src/pagos/model/pagos.model.js
+++ src/pagos/model/pagos.model.js
@@ -1,71 +1,56 @@
 const pool = require("../../model/connection-database")
 
 
-class PagosModel {
 
-    async getPagoByCodigo(CODIGO) {
-        const [ficha_data] = await pool.query(
-            "SELECT * FROM `PagosSV` WHERE CODIGO = ?;"
-            , [CODIGO]);
+const getPagoByCodigo = async (CODIGO) => {
+    const [ficha_data] = await pool.query(
+        "SELECT * FROM `PagosSV` WHERE CODIGO = ?;"
+        , [CODIGO]);
 
-        if (ficha_data.length > 0) {
-            return ficha_data[0];
-        }
-        return [];
+    if (ficha_data.length > 0) {
+        return ficha_data[0];
     }
-    // deprecated
-    // async getFicha(FICHA) {
-
-    //     const [ficha_data] = await pool.query(
-    //         "SELECT Fichas.CTE,Fichas.FICHA,Fichas.PRIMER_PAGO,Fichas.VENCIMIENTO,Fichas.TOTAL,Fichas.SERVICIO_ANT," +
-    //         "CONVERT(IFNULL(SUM(PagosSV.SERV),0),INTEGER) as SERV_PAGO, SERV_UNIT,OBS,CUOTA,CUOTA_ANT," +
-    //         "Fichas.CUOTA_ANT - CONVERT(IFNULL(sum(PagosSV.VALOR),0),INTEGER) as SALDO, CONVERT(Fichas.TOTAL / Fichas.CUOTA,INTEGER) as CUOTAS, " +
-    //         "CONVERT(IFNULL(SUM(PagosSV.VALOR),0),INTEGER) as CUOTA_PAGO,Fichas.MORA_ANT, CONVERT(IFNULL(sum(PagosSV.MORA),0),INTEGER) as MORA_PAGO FROM `Fichas` " +
-    //         "left join PagosSV on PagosSV.FICHA = Fichas.FICHA where Fichas.FICHA = ? ;"
-    //         , [FICHA]);
-
-    //     if (ficha_data.length > 0) {
-
-    //         return ficha_data[0];
-    //     }
-
-    //     return [];
-
-    // }
-    async cargarPago({ CTE, FICHA, CUOTA, PROXIMO, SERV = 0, MORA = 0,
-        CONFIRMACION = "PENDIENTE", USUARIO, FECHA, CODIGO, OBS, MP_PORCENTAJE = 0, N_OPERACION, MP_TITULAR, DECLARADO_COB = 0, DECLARADO_CUO = 0, ID_VENTA = null }) {
-        const connection = await pool.getConnection();
-
-        try {
-            await connection.beginTransaction();
-
-            const [ficha_data] = await connection.query(
-                `INSERT INTO PagosSV (CTE, FICHA, VALOR, PROXIMO, MP, SERV, MORA, COBRADOR, FECHA, CONFIRMACION,CODIGO,OBS,MP_OPERACION,MP_TITULAR,DECLARADO_COB,DECLARADO_CUO,ID_VENTA) SELECT ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,? where not EXISTS (SELECT true from PagosSV where ID_VENTA = ?)`
-                , [CTE, FICHA, CUOTA, PROXIMO, MP_PORCENTAJE, SERV, MORA, USUARIO, FECHA, CONFIRMACION, CODIGO, OBS, N_OPERACION, MP_TITULAR, DECLARADO_COB, DECLARADO_CUO, ID_VENTA, ID_VENTA]);
+    return [];
+}
 
 
-            await connection.query(
-                `INSERT INTO PlanillasDeCobranza 
+
+const cargarPago = async ({
+    CTE, FICHA, CUOTA, PROXIMO, SERV = 0, MORA = 0,
+    CONFIRMACION = "PENDIENTE", USUARIO, FECHA, CODIGO, OBS, MP_PORCENTAJE = 0, N_OPERACION, MP_TITULAR, DECLARADO_COB = 0, DECLARADO_CUO = 0, ID_VENTA = null
+}) => {
+    const connection = await pool.getConnection();
+
+    try {
+        await connection.beginTransaction();
+
+        const [ficha_data] = await connection.query(
+            `INSERT INTO PagosSV (CTE, FICHA, VALOR, PROXIMO, MP, SERV, MORA, COBRADOR, FECHA, CONFIRMACION,CODIGO,OBS,MP_OPERACION,MP_TITULAR,DECLARADO_COB,DECLARADO_CUO,ID_VENTA) SELECT ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,? where not EXISTS (SELECT true from PagosSV where ID_VENTA = ?)`
+            , [CTE, FICHA, CUOTA, PROXIMO, MP_PORCENTAJE, SERV, MORA, USUARIO, FECHA, CONFIRMACION, CODIGO, OBS, N_OPERACION, MP_TITULAR, DECLARADO_COB, DECLARADO_CUO, ID_VENTA, ID_VENTA]);
+
+
+        await connection.query(
+            `INSERT INTO PlanillasDeCobranza 
                 (FECHA,COB, EDITABLE,EFECTIVO, RECEPCION) SELECT 
                 ? where not EXISTS (SELECT true from PlanillasDeCobranza where FECHA = ? and COB = ?);`
-                , [[FECHA, USUARIO, 1, 0, null], FECHA, USUARIO]);
+            , [[FECHA, USUARIO, 1, 0, null], FECHA, USUARIO]);
 
-            await connection.commit();
-            return ficha_data;
+        await connection.commit();
+        return ficha_data;
 
-        } catch (error) {
-            console.error(error);
-            await connection.rollback();
-        } finally {
-            connection.release();
-        }
-        return [];
-
+    } catch (error) {
+        console.error(error);
+        await connection.rollback();
+    } finally {
+        connection.release();
     }
+    return [];
 
-    async getFichasByCte(CTE = "%", MODO = "CTE") {
-        const [fichas] = await pool.query(
-            `SELECT
+}
+
+const getFichasByCte = async (CTE = "%", MODO = "CTE") => {
+    const [fichas] = await pool.query(
+        `SELECT
             Fichas.FECHA AS FECHA_VENTA,
             Fichas.CTE,
             Fichas.PRIMER_PAGO,
@@ -110,38 +95,38 @@ class PagosModel {
             Fichas.?? LIKE ? 
         GROUP BY
             Fichas.FICHA;`
-            // HAVING
-            //     SALDO > 0;
-            , [MODO, CTE]);
+        // HAVING
+        //     SALDO > 0;
+        , [MODO, CTE]);
 
-        if (fichas.length > 0) {
-            return fichas;
-        }
-
-        return [];
-    }
-    async getPrestamosByCte(CTE, MODO = "CTE") {
-        const query = "SELECT `FECHA`, `CTE`, `Prestamo`, `Zona`, `Valor`, `Capital`, `Ant`, `Mes 0`, `Mes 1`, `Mes 2`, `Mes 3`, `Mes 4`, `Mes 5`, `Saldo Ant`, `Mes 6`, `Saldo Act`, CONVERT(`Cuota`,INTEGER) as `CUOTA`, `Cuo`, `Estatus`, `V`, `Fecha cobro`, `C De Fecha`, `Prox Fecha`, `SERVICIOS ANT`, `SERVICIOS PAGO`, CONVERT(`SERVICIOS`,INTEGER) AS `SERVICIOS`, CONVERT(`MORA`,INTEGER) AS `MORA`, `MORA UNIT`, `Vencidas`, CONVERT(`Deuda Cuo`,INTEGER) AS `DEUDA_CUO` FROM `CobranzasEC` WHERE ?? = ?  UNION SELECT `FECHA`, `CTE`, `Prestamo`, `Zona`, `Valor`, `Capital`, `Ant`, `Mes 0`, `Mes 1`, `Mes 2`, `Mes 3`, `Mes 4`, `Mes 5`, `Saldo Ant`, `Mes 6`, `Saldo Act`, CONVERT(`Cuota`,INTEGER) as `CUOTA`, `Cuo`, `Entregado`, `V`, `Fecha cobro`, `C De Fecha`, `Prox Fecha`, `SERVICIOS ANT`,`SERVICIOS PAGO`,`SERVICIOS`,  `MORA`, `MORA UNIT`, `Vencidas`, `Deuda Cuo` FROM `VentasEC` WHERE ?? = ?;"
-
-        const [prestamos] = await pool.query(
-            query, [MODO, CTE, MODO, CTE]
-        );
-
-        return prestamos
+    if (fichas.length > 0) {
+        return fichas;
     }
 
-    async insertCambioDeFecha({ CTE, FICHA, FECHA_COB, COBRADOR, FECHA }) {
-        const [response] = await pool.query(
-            "INSERT INTO `PagosSV` (`CTE`, `FICHA`, `PROXIMO`, `COBRADOR`, `FECHA`) " +
-            "VALUES " +
-            "(?,?,?,?,?)", [CTE, FICHA, FECHA_COB, COBRADOR, FECHA])
+    return [];
+}
+const getPrestamosByCte = async (CTE, MODO = "CTE") => {
+    const query = "SELECT `FECHA`, `CTE`, `Prestamo`, `Zona`, `Valor`, `Capital`, `Ant`, `Mes 0`, `Mes 1`, `Mes 2`, `Mes 3`, `Mes 4`, `Mes 5`, `Saldo Ant`, `Mes 6`, `Saldo Act`, CONVERT(`Cuota`,INTEGER) as `CUOTA`, `Cuo`, `Estatus`, `V`, `Fecha cobro`, `C De Fecha`, `Prox Fecha`, `SERVICIOS ANT`, `SERVICIOS PAGO`, CONVERT(`SERVICIOS`,INTEGER) AS `SERVICIOS`, CONVERT(`MORA`,INTEGER) AS `MORA`, `MORA UNIT`, `Vencidas`, CONVERT(`Deuda Cuo`,INTEGER) AS `DEUDA_CUO` FROM `CobranzasEC` WHERE ?? = ?  UNION SELECT `FECHA`, `CTE`, `Prestamo`, `Zona`, `Valor`, `Capital`, `Ant`, `Mes 0`, `Mes 1`, `Mes 2`, `Mes 3`, `Mes 4`, `Mes 5`, `Saldo Ant`, `Mes 6`, `Saldo Act`, CONVERT(`Cuota`,INTEGER) as `CUOTA`, `Cuo`, `Entregado`, `V`, `Fecha cobro`, `C De Fecha`, `Prox Fecha`, `SERVICIOS ANT`,`SERVICIOS PAGO`,`SERVICIOS`,  `MORA`, `MORA UNIT`, `Vencidas`, `Deuda Cuo` FROM `VentasEC` WHERE ?? = ?;"
 
-        return response;
-    }
-    async getAcumuladoByCteFicha({ CTE, FICHA }) {
+    const [prestamos] = await pool.query(
+        query, [MODO, CTE, MODO, CTE]
+    );
 
-        const [pago_data] = await pool.query(
-            `SELECT
+    return prestamos
+}
+
+const insertCambioDeFecha = async ({ CTE, FICHA, FECHA_COB, COBRADOR, FECHA }) => {
+    const [response] = await pool.query(
+        "INSERT INTO `PagosSV` (`CTE`, `FICHA`, `PROXIMO`, `COBRADOR`, `FECHA`) " +
+        "VALUES " +
+        "(?,?,?,?,?)", [CTE, FICHA, FECHA_COB, COBRADOR, FECHA])
+
+    return response;
+}
+const getAcumuladoByCteFicha = async ({ CTE, FICHA }) => {
+
+    const [pago_data] = await pool.query(
+        `SELECT
         MES,CONVERT(SUM(CUOTA),INTEGER) as CUOTA,CONVERT(SUM(t.MORA),INTEGER) AS MORA,CONVERT(sum(t.SERV),INTEGER) AS SERV
     FROM
         (
@@ -183,97 +168,97 @@ class PagosModel {
     ) t
     GROUP BY
         t.MES;`
-            , [CTE, FICHA, CTE, FICHA])
+        , [CTE, FICHA, CTE, FICHA])
 
-        if (pago_data.length > 0) {
-            return pago_data;
-        }
-
-        return [];
-
+    if (pago_data.length > 0) {
+        return pago_data;
     }
 
+    return [];
+
+}
 
 
-    async getFechasDePagosYCobradores() {
 
-        const [FECHAS] = await pool.query(
-            "SELECT DISTINCT COBRADOR,FECHA FROM `PagosSV` WHERE CONFIRMACION != 'INVALIDO' ORDER BY `PagosSV`.`FECHA` DESC;");
+const getFechasDePagosYCobradores = async () => {
+
+    const [FECHAS] = await pool.query(
+        "SELECT DISTINCT COBRADOR,FECHA FROM `PagosSV` WHERE CONFIRMACION != 'INVALIDO' ORDER BY `PagosSV`.`FECHA` DESC;");
 
 
-        if (FECHAS.length > 0) {
-            return FECHAS;
-        }
-
-        return [];
-
+    if (FECHAS.length > 0) {
+        return FECHAS;
     }
 
-    async getPagosByFechaYCob({ COB = "%", FECHA = "%", ORDEN }) {
+    return [];
 
-        const [PAGOS] = await pool.query(
-            "SELECT PagosSV.`CTE`, PagosSV.`FICHA`,Clientes.ZONA as Z, `VALOR` AS CUOTA,DAY(Fichas.VENCIMIENTO) as DIA_VENCIMIENTO, `PROXIMO`, " +
-            "PagosSV.`OBS` , `MP`, `SERV`, `MORA`, `COBRADOR`, PagosSV.`FECHA`,PagosSV.`MP_TITULAR`, `CONFIRMACION`, `CODIGO`, " +
-            "PagosSV.`ID`, Fichas.CUOTA_ANT - (SELECT SUM(PagosSV.VALOR) FROM PagosSV " +
-            "Where PagosSV.FICHA = Fichas.FICHA and PagosSV.CONFIRMACION != 'INVALIDO') as SALDO,PagosSV.DECLARADO_CUO,PagosSV.DECLARADO_COB , PagosSV.SERV + PagosSV.MORA as CUOTA_SERV, Clientes.CALLE,Clientes.`APELLIDO Y NOMBRE` AS NOMBRE " +
-            "FROM `PagosSV` left join Fichas on Fichas.FICHA = PagosSV.FICHA left join Clientes on Clientes.CTE = PagosSV.CTE where PagosSV.FECHA " +
-            "like ? and PagosSV.COBRADOR like ? AND PagosSV.CONFIRMACION != 'INVALIDO' group by CODIGO order by CONFIRMACION, ?? ,PagosSV.FICHA; "
-            , [FECHA, COB, ORDEN]);
+}
 
-        if (PAGOS.length > 0) {
-            return PAGOS;
-        }
-        return [];
+const getPagosByFechaYCob = async ({ COB = "%", FECHA = "%", ORDEN }) => {
+
+    const [PAGOS] = await pool.query(
+        "SELECT PagosSV.`CTE`, PagosSV.`FICHA`,Clientes.ZONA as Z, `VALOR` AS CUOTA,DAY(Fichas.VENCIMIENTO) as DIA_VENCIMIENTO, `PROXIMO`, " +
+        "PagosSV.`OBS` , `MP`, `SERV`, `MORA`, `COBRADOR`, PagosSV.`FECHA`,PagosSV.`MP_TITULAR`, `CONFIRMACION`, `CODIGO`, " +
+        "PagosSV.`ID`, Fichas.CUOTA_ANT - (SELECT SUM(PagosSV.VALOR) FROM PagosSV " +
+        "Where PagosSV.FICHA = Fichas.FICHA and PagosSV.CONFIRMACION != 'INVALIDO') as SALDO,PagosSV.DECLARADO_CUO,PagosSV.DECLARADO_COB , PagosSV.SERV + PagosSV.MORA as CUOTA_SERV, Clientes.CALLE,Clientes.`APELLIDO Y NOMBRE` AS NOMBRE " +
+        "FROM `PagosSV` left join Fichas on Fichas.FICHA = PagosSV.FICHA left join Clientes on Clientes.CTE = PagosSV.CTE where PagosSV.FECHA " +
+        "like ? and PagosSV.COBRADOR like ? AND PagosSV.CONFIRMACION != 'INVALIDO' group by CODIGO order by CONFIRMACION, ?? ,PagosSV.FICHA; "
+        , [FECHA, COB, ORDEN]);
+
+    if (PAGOS.length > 0) {
+        return PAGOS;
     }
+    return [];
+}
 
-    async updateDistribucionByCodigo({ PROXIMO, SERV, MORA, CUOTA, CODIGO }) {
+const updateDistribucionByCodigo = async ({ PROXIMO, SERV, MORA, CUOTA, CODIGO }) => {
+    const [update_result] = await pool.query(
+        "UPDATE PagosSV SET PROXIMO = ?, SERV = ? , MORA = ?, VALOR = ? WHERE CODIGO = ? ", [PROXIMO, SERV, MORA, CUOTA, CODIGO]);
+
+    if (update_result.length > 0) {
+        return update_result;
+    }
+    return [];
+}
+const updateEstadoPagoByCodigo = async ({ newState, filter }) => {
+    const [update_result] = await pool.query
+        (`UPDATE PagosSV SET ? WHERE ? `, [newState, filter]);
+
+    if (update_result.length > 0) {
+        return update_result;
+    }
+    return [];
+}
+
+
+const getFichas = async () => {
+    const [FICHAS] = await pool.query(
+        "SELECT `FECHA`, `CTE`, `FICHA`, `Z`, `TOTAL`, `CUOTA`, " +
+        "`VENCIMIENTO`, `CUOTA_ANT`, `SERVICIO_ANT`, `MORA_ANT`, `SERV_UNIT`, " +
+        "`ARTICULOS`, `ID` FROM `Fichas` ");
+
+    if (FICHAS.length > 0) {
+        return FICHAS;
+    }
+    return [];
+
+}
+
+const updateMoraYServicioAntBase = async () => {
+    const [update_result] = await pool.query(
+        "UPDATE PagosSV SET CONFIRMACION = ? WHERE CODIGO = ? ", [ESTADO, CODIGO]);
+
+    if (update_result.length > 0) {
+        return update_result;
+    }
+    return [];
+}
+
+//Recibe un array de numeros de fichas
+const updateSaldosAnterioresYServicios = async (FICHAS) => {
+    try {
         const [update_result] = await pool.query(
-            "UPDATE PagosSV SET PROXIMO = ?, SERV = ? , MORA = ?, VALOR = ? WHERE CODIGO = ? ", [PROXIMO, SERV, MORA, CUOTA, CODIGO]);
-
-        if (update_result.length > 0) {
-            return update_result;
-        }
-        return [];
-    }
-    async updateEstadoPagoByCodigo({ newState, filter }) {
-        const [update_result] = await pool.query
-            (`UPDATE PagosSV SET ? WHERE ? `, [newState, filter]);
-
-        if (update_result.length > 0) {
-            return update_result;
-        }
-        return [];
-    }
-
-
-    async getFichas() {
-        const [FICHAS] = await pool.query(
-            "SELECT `FECHA`, `CTE`, `FICHA`, `Z`, `TOTAL`, `CUOTA`, " +
-            "`VENCIMIENTO`, `CUOTA_ANT`, `SERVICIO_ANT`, `MORA_ANT`, `SERV_UNIT`, " +
-            "`ARTICULOS`, `ID` FROM `Fichas` ");
-
-        if (FICHAS.length > 0) {
-            return FICHAS;
-        }
-        return [];
-
-    }
-
-    async updateMoraYServicioAntBase() {
-        const [update_result] = await pool.query(
-            "UPDATE PagosSV SET CONFIRMACION = ? WHERE CODIGO = ? ", [ESTADO, CODIGO]);
-
-        if (update_result.length > 0) {
-            return update_result;
-        }
-        return [];
-    }
-
-    //Recibe un array de numeros de fichas
-    async updateSaldosAnterioresYServicios(FICHAS) {
-        try {
-            const [update_result] = await pool.query(
-                `UPDATE
+            `UPDATE
                 Fichas
             LEFT JOIN(
                 SELECT
@@ -296,16 +281,16 @@ class PagosModel {
                 Fichas.MORA_ANT = subconsulta.suma_mora
             WHERE
                 Fichas.FICHA IN(?)`, [FICHAS]);
-            return update_result;
+        return update_result;
 
-        } catch (err) {
-            console.error("ERROR AL CARGAR SALDOS ANTERIORES", err);
-        }
+    } catch (err) {
+        console.error("ERROR AL CARGAR SALDOS ANTERIORES", err);
     }
-
-
 }
 
 
 
-module.exports = { pagosModel: new PagosModel() }
+
+
+
+module.exports = { cargarPago, getAcumuladoByCteFicha, getFechasDePagosYCobradores, getFichas, getFichasByCte, getPagoByCodigo, getPagosByFechaYCob, getPrestamosByCte, insertCambioDeFecha }
