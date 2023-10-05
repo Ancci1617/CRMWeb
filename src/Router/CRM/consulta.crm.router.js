7d363e007efe2772cda18436ea63c5651e31dafe
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-10-05 16:43:30 -0300

    Cambia la consulta de CRM en Fichas

--- src/Router/CRM/consulta.crm.router.js
+++ src/Router/CRM/consulta.crm.router.js
@@ -8,6 +8,8 @@ const { getMasterBGM, getMasterEC, getMasterResumen } = require("../../model/CRM
 const { getDomicilio } = require("../../model/CRM/get_tablas/get_domicilio.js");
 const { isLoggedIn } = require("../../lib/auth");
 const { guardar_respuesta_crm } = require("../../model/CRM/guardar-consulta.js");
+const { getVencidas, getToday } = require("../../lib/dates.js");
+const { getDoubt } = require("../../lib/doubt.js");
 const express = require("express");
 const path = require("path");
 
@@ -17,7 +19,7 @@ Router.use(isLoggedIn, express.static(path.join("..", "ImagenesDeClientes")));
 
 Router.get("/CRM", isLoggedIn, (req, res) => {
     const CTE = req.query.CTE;
-    res.render("CRM/CRM.ejs", {CTE});
+    res.render("CRM/CRM.ejs", { CTE });
 });
 
 
@@ -40,6 +42,24 @@ Router.post("/query_CRM", isLoggedIn, async (req, res) => {
 
     query_result.Clientes = await getClientes(cte);
     query_result.Fichas = await getFichas(cte);
+
+    //Agregar vencidas,pagas,totales,atrasos;
+    query_result.Fichas = query_result.Fichas.map(ficha => {
+        const { FECHA_FORMAT, FICHA, Z, TOTAL, ANT, MES0, MES1, MES2, MES3, MES4, MES5, CUOTA_ANT, CUOTA_PAGO, SALDO, CUOTA, VU, VENCIMIENTO, CDeFecha, PRIMER_PAGO, CUOTAS, SERVICIO_ANT, SERV_PAGO, SERV_UNIT, MORA_ANT, MORA_PAGO, FECHA } = ficha;
+        const deuda = getDoubt({
+            VENCIMIENTO, PRIMER_PAGO, CUOTAS, CUOTA, TOTAL, CUOTA_ANT, CUOTA_PAGO, SALDO,
+            SERVICIO_ANT, Z, FECHA_VENTA: FECHA, SERV_PAGO, SERV_UNIT, MORA_ANT, MORA_PAGO
+        })
+
+
+        const { vencidas, pagas, atraso } = deuda;
+        return { FECHA_FORMAT, FICHA, Z, TOTAL, ANT, MES0, MES1, MES2, MES3, MES4, MES5, CUOTA_ANT, CUOTA_PAGO, SALDO, CUOTA, VU, VENCIMIENTO, CDeFecha, vencidas, pagas, CUOTAS, atraso }
+
+    }
+    )
+
+    console.log("Fichasfinal",query_result.Fichas);
+
     query_result.Prestamos = await getPrestamos(cte);
     query_result.MasterBGM = await getMasterBGM(cte);
     query_result.MasterEC = await getMasterEC(cte);
