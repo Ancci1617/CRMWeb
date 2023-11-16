806f8ed6abdc2d484ee2df1bac64970796cb2567
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-11-16 09:34:04 -0300

    Puede eliminar imagenes y cargar imagenes de clientes sin carpeta

--- src/Router/ventas/dnis/dnis.router.js
+++ src/Router/ventas/dnis/dnis.router.js
@@ -1,6 +1,5 @@
 const Router = require("express").Router();
 const path = require("path");
-const { isLoggedIn } = require("../../../lib/auth");
 const fs = require("fs/promises");
 const { saveFileFromEntry } = require("../../../lib/files");
 const { hasPermission } = require("../../../middlewares/permission.middleware.js");
@@ -11,13 +10,13 @@ const { getListadoDeNumerosDeClientes } = require("../../../shared/model/Cliente
 Router.get("/", hasPermission(permisos.DNI_USER), async (req, res) => {
 
     const clientes = await getListadoDeNumerosDeClientes();
-    const links = clientes.map(cte => `/dnis/${cte.CTE}`);
+    const links = clientes.map(cte => `/dnis/cargar_imagen/${cte.CTE}`);
     const items = clientes.map(cte => cte.CTE);
     const data = { title: "Dnis", items, links };
     res.render("list-items.ejs", { data });
 });
 
-Router.get("/:CTE", hasPermission(permisos.DNI_USER), async (req, res) => {
+Router.get("/cargar_imagen/:CTE", hasPermission(permisos.DNI_USER), async (req, res) => {
     const { CTE } = req.params;
     res.render("ventas/dnis/dnis.visualizar.ejs", { CTE });
 });
@@ -28,17 +27,20 @@ Router.post("/cargar_imagen/:CTE", async (req, res) => {
     const { CTE } = req.params;
     const entries = Object.entries(req.files);
     saveFileFromEntry(entries, CTE);
-    res.redirect(`/dnis/${CTE}`);
+    res.redirect(`/dnis/cargar_imagen/${CTE}`);
 });
 
 Router.get("/eliminar_imagen/", async (req, res) => {
     const { CTE, IMAGEN } = req.query;
     if (!CTE || !IMAGEN) return res.status(400).send("Parametros incorrectos.");
+    try {
+        await fs.unlink(`../ImagenesDeClientes/${CTE}/CTE-${CTE}-${IMAGEN}.jpg`);
+    } catch (error) {
+        console.log(error);
+        return res.send("No se encontro la imagen para eliminar");
+    }
+    res.redirect(`/dnis/cargar_imagen/${CTE}`);
 
-    const resultado = await fs.unlink(`../ImagenesDeClientes/${CTE}/CTE-${CTE}-${IMAGEN}.jpg`);
-    console.log("🚀 ~ file: dnis.router.js:39 ~ Router.get ~ resultado:", resultado)
-
-    res.send("ok")
 });
 
 
