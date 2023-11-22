56caf1f7bd3d6a41eea6645d47ed488b0383dd2b
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-11-22 10:26:16 -0300

    cambia a base local

--- src/model/connection-object.js
+++ src/model/connection-object.js
@@ -1,19 +1,19 @@
-const connection_data = {
-    host: 'srv815.hstgr.io',
-    port : 3306,
-    user: 'u970133903_BGMAdmin',
-    password: 'RKfwnmPhW1!l',
-    database: 'u970133903_BGMDB'
-}
+// const connection_data = {
+//     host: 'srv815.hstgr.io',
+//     port : 3306,
+//     user: 'u970133903_BGMAdmin',
+//     password: 'RKfwnmPhW1!l',
+//     database: 'u970133903_BGMDB'
+// }
 
 //LOCAL
-// const connection_data = {
-//     host: 'localhost', //srv815.hstgr.io
-//      port: 3306,
-//      user: 'root',
-//      password: '',
-//      database: 'bgm_db'
-// }
+const connection_data = {
+    host: 'localhost', //srv815.hstgr.io
+     port: 3306,
+     user: 'root',
+     password: '',
+     database: 'bgm_db'
+}
 
 
 //DE PRUEBA
