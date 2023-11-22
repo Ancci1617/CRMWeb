8f1b0092e1b381738195f9086eabb146b377df72
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-11-22 10:17:35 -0300

    cambia la distribucion de los pagos y el maximo de deuda posible y calculable por servicio es de 2 servicios

--- src/pagos/lib/redistribuciones.js
+++ src/pagos/lib/redistribuciones.js
@@ -12,32 +12,19 @@ const redistribuirPagoBgm = async ({ FICHA, COBRADO, DECLARADO_CUO, DECLARADO_CO
 
 
     let MORA = [0], SERV = [0], CUOTA = [0];
+    MORA[0] = Math.max(Math.min(ficha.deuda.mora, COBRADO),0);
+    COBRADO -= MORA[0];
 
-
-    let servicios_adeudados = ficha.deuda.servicio;
     for (let i = 0; i < ficha.deuda.atraso_evaluado; i++) {
-        MORA[i] = 0;
         SERV[i] = 0;
         CUOTA[i] = 0;
 
 
+        SERV[i] += Math.max(Math.min(ficha.deuda.servicio, ficha.data.SERV_UNIT, COBRADO), 0);
+        ficha.deuda.servicio -= SERV[i];
+        COBRADO -= SERV[i];
 
-        MORA[i] += Math.max(Math.min(ficha.deuda.mora, Math.floor(ficha.data.CUOTA * 0.1 / 100) * 100, COBRADO), 0); // 600
-        ficha.deuda.mora -= MORA[i];
-        COBRADO -= MORA[i]; //i = 0 ; = 4400
-
-
-        if (!(servicios_adeudados / ficha.data.SERV_UNIT < ficha.deuda.atraso_evaluado - i)) {
-
-            SERV[i] += Math.max(Math.min(ficha.deuda.servicio, ficha.data.SERV_UNIT, COBRADO), 0); // 1000
-            ficha.deuda.servicio -= SERV[i];
-            COBRADO -= SERV[i]; // 3400
-
-        }
-
-
-
-        CUOTA[i] += Math.max(Math.min(ficha.deuda.cuota, ficha.data.CUOTA, COBRADO), 0); //4400
+        CUOTA[i] += Math.max(Math.min(ficha.deuda.cuota, ficha.data.CUOTA, COBRADO), 0);
         ficha.deuda.cuota -= CUOTA[i];
         COBRADO -= CUOTA[i];
 
