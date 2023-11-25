db51df23163f87f9e0737622e01a074181f63f4c
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-11-25 12:46:22 -0300

    Agrega los permisos en el enrutador admin

--- src/admin/routes/admin.routes.js
+++ src/admin/routes/admin.routes.js
@@ -4,26 +4,34 @@ const { validateSchema } = require("../../shared/middlewares/validateSchema.js")
 const { clienteSchema, fichasSchema } = require("../schema/admin.schema.js");
 const { validateParamsNCTE } = require("../middlewares/admin.data.validation.js");
 const { destructurarUbicacion } = require("../../ubicaciones/middleware/ubicaciones.middleware.js");
-const {hasPermission} =require("../../middlewares/permission.middleware.js");
+const { hasPermission } = require("../../middlewares/permission.middleware.js");
 const permisos = require("../../constants/permisos.js");
 
 
 const router = Router();
 
 //Puede entrar a ver los datos si almenos tiene el permiso de admin_cob
-router.get("/editarCliente/:cte", validateParamsNCTE,hasPermission(permisos.ADMIN_COB), adminController.editarClienteForm);
+router.get("/editarCliente/:cte", validateParamsNCTE, hasPermission(permisos.ADMIN_COB), adminController.editarClienteForm);
 
 router.post("/editarCliente/:cte",
+    hasPermission(permisos.ADMIN_COB),
     validateParamsNCTE,
     validateSchema(clienteSchema),
     destructurarUbicacion,
     adminController.editarClientePost);
 
-router.get("/editarFicha/:FICHA",adminController.editarFichaForm);
+router.get("/editarFicha/:FICHA",
+    hasPermission(permisos.ADMIN_COB),
+    adminController.editarFichaForm);
 
-router.post("/editarFicha/:FICHA",validateSchema(fichasSchema),adminController.editarFichaPost);
+router.post("/editarFicha/:FICHA",
+    hasPermission(permisos.ADMIN_COB),
+    validateSchema(fichasSchema),
+    adminController.editarFichaPost);
 
-router.get("/cargarDevolucion/:FICHA",adminController.cargarDevolucion);
+router.get("/cargarDevolucion/:FICHA",
+    hasPermission(permisos.ADMIN_ADMIN),
+    adminController.cargarDevolucion);
 
 
 
