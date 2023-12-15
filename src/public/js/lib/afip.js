ed653ba11fee765a04d38197699acd8caffe3f93
Rodrigo Vazquez <blanco.gusmar@gmail.com>
2023-12-15 10:52:27 -0300

    Vendedores no pueden editar DNI + Chekea razon social al momento de escribir el nombre o al momento de escribir el dni

--- src/public/js/lib/afip.js
+++ src/public/js/lib/afip.js
@@ -1,10 +1,11 @@
-async function getRazonSocialDni(dni){
+async function getRazonSocialDni(dni) {
     //Consulta el CUIT Con el Dni del parametros
     const response_cuit = await fetch(`https://afip.tangofactura.com/Index/GetCuitsPorDocumento/?NumeroDocumento=${dni}`);
     const response_cuit_json = await response_cuit.json();
 
-    if(response_cuit_json.error) 
+    if (response_cuit_json.error) {
         throw new Error(response_cuit_json.error)
+    }
 
 
     //cONSULTA EL CONTRIBUYENTE CON EL CUIT
@@ -12,11 +13,11 @@ async function getRazonSocialDni(dni){
     const response_contribuyente = await fetch(`https://afip.tangofactura.com/Index/GetContribuyente/?cuit=${cuits[0]}`);
     const response_contribuyente_json = await response_contribuyente.json();
 
-    if(response_contribuyente_json.errorGetData == true || response_contribuyente_json.error)
+    if (response_contribuyente_json.errorGetData == true || response_contribuyente_json.error)
         throw new Error("No se pudo consultar la razon social")
     //=nomPropio de excel
-    const {nombre} = response_contribuyente_json.Contribuyente
-    const nombre_prop = nombre.toLocaleLowerCase().split(" ").map(word => word.replace(word.charAt(0),word.charAt(0).toUpperCase())  ).join(" ");
+    const { nombre } = response_contribuyente_json.Contribuyente
+    const nombre_prop = nombre.toLocaleLowerCase().split(" ").map(word => word.replace(word.charAt(0), word.charAt(0).toUpperCase())).join(" ");
     return nombre_prop;
 
 }
