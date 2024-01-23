a312e9c1e6d24d45c78f9035f11af9366e016ad5
ancci16172 <rodrigovazquez1617@gmail.com>
2024-01-23 15:41:54 -0300

    Mueve a base remota

--- src/model/connection-object.js
+++ src/model/connection-object.js
@@ -1,22 +1,22 @@
-// const connection_data = {
-//     host: 'srv815.hstgr.io',
-//     port : 3306,
-//     user: 'u970133903_BGMAdmin',
-//     password: 'RKfwnmPhW1!l',
-//     database: 'u970133903_BGMDB',
-//     timezone : "+00:00" //Aca va la hora GMT a la cual esta configurado el servidor
-// } 
+const connection_data = {
+    host: 'srv815.hstgr.io',
+    port : 3306,
+    user: 'u970133903_BGMAdmin',
+    password: 'RKfwnmPhW1!l',
+    database: 'u970133903_BGMDB',
+    timezone : "+00:00" //Aca va la hora GMT a la cual esta configurado el servidor
+} 
 
 //LOCAL
-const connection_data = {
-    host: 'localhost', //srv815.hstgr.io
-     port: 3306,
-     user: 'root',
-     password: '',
-     database: 'bgm_db',
-     timezone : "+03:00"
+// const connection_data = {
+//     host: 'localhost', //srv815.hstgr.io
+//      port: 3306,
+//      user: 'root',
+//      password: '',
+//      database: 'bgm_db',
+//      timezone : "+03:00"
 
-}
+// }
 
 
 //DE PRUEBA
