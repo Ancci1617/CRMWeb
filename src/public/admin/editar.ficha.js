8e81b60de855547a2cfc26357618b278adff9fbb
ancci16172 <rodrigovazquez1617@gmail.com>
2024-01-23 17:44:07 -0300

    EL total NO ES DIVISIBLE, eliminado

--- src/public/admin/editar.ficha.js
+++ src/public/admin/editar.ficha.js
@@ -2,16 +2,16 @@
 const form = document.querySelector("form")
 
 //Valida el total sea divisible por la cuota
-form.addEventListener("submit", e => {
-    const { TOTAL: { value: TOTAL }, CUOTA: { value: CUOTA } } = e.target;
+// form.addEventListener("submit", e => {
+//     const { TOTAL: { value: TOTAL }, CUOTA: { value: CUOTA } } = e.target;
 
-    if (parseInt(TOTAL) % parseInt(CUOTA) != 0) {
-        e.preventDefault();
-        return alert("El total no es divisible por las cuotas..")
-    }
+//     if (parseInt(TOTAL) % parseInt(CUOTA) != 0) {
+//         e.preventDefault();
+//         return alert("El total no es divisible por las cuotas..")
+//     }
 
 
-})
+// })
 
 
 
