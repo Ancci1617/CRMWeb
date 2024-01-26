10e6f192bf3b502a820e76654bdf38f08623e348
ancci16172 <rodrigovazquez1617@gmail.com>
2024-01-26 11:30:33 -0300

    #Bug fix - No funciona bien el Colapsar al estar en la pantalla de deuda_cte

--- src/model/CRM/get_tablas/get_articulos.js
+++ src/model/CRM/get_tablas/get_articulos.js
@@ -6,12 +6,22 @@ const getArticulosString = async (articulos) => {
     //articutlos = ["34","35","36"]
     try {
         const [rows] = await pool.query(
-            `SELECT Producto FROM LP WHERE Art IN (?);`, [articulos]);
+            `SELECT Producto,Art FROM LP WHERE Art IN (?);`, [articulos]);
 
         if (rows.length > 0) {
-            let reduced = rows.reduce((acum, art) => acum + art.Producto + "-", "");
+
+            const reduced = articulos.reduce((acum, art) => {
+                const foundArt = rows.find(row => row.Art == art) || { Producto: "No encontrado" };
+
+                return acum + foundArt.Producto + " | "
+
+            }, "");
             return reduced.substring(0, reduced.length - 1);
+
         }
+
+
+
     } catch (error) {
         console.log("error al consultar articulos", error);
     }
