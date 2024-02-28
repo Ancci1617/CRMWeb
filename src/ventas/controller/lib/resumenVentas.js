9363991afecffadd6eb04e36dc490fd996550662
ancci16172 <rodrigovazquez1617@gmail.com>
2024-02-28 16:03:03 -0300

    Agrega cantidad de fichas vendidas por vendeor en resumen general de ventas

--- src/ventas/controller/lib/resumenVentas.js
+++ src/ventas/controller/lib/resumenVentas.js
@@ -2,21 +2,29 @@
 
 const generarResumenDeVentas = (ventas) => {
 
-    return ventas.reduce((acum, venta) => {
+    const resumen = ventas.reduce((acum, venta) => {
         const { USUARIO, TOTAL, ARTICULOS } = venta;
 
-        if (!acum.VENDEDORES[USUARIO]) acum.VENDEDORES[USUARIO] = 0;
+        if (!acum.VENDEDORES[USUARIO]) {
+            acum.VENDEDORES[USUARIO] = { TOTAL: 0, CANTIDAD: 0 };
+        }
 
-        acum.VENDEDORES[USUARIO] += TOTAL
+        acum.VENDEDORES[USUARIO] = {
+            TOTAL: acum.VENDEDORES[USUARIO].TOTAL + TOTAL,
+            CANTIDAD: acum.VENDEDORES[USUARIO].CANTIDAD + 1
+        }
 
 
         return {
-            ...acum, 
+            ...acum,
             TOTAL: acum.TOTAL + TOTAL,
             COLOCADO: acum.COLOCADO + parseInt(ARTICULOS)
         }
 
-    }, { TOTAL: 0, COLOCADO: 0 ,VENDEDORES : {}})
+    }, { TOTAL: 0, COLOCADO: 0, VENDEDORES: {} })
+
+
+    return resumen
 }
 
 
